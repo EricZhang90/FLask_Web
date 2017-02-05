5b3ed76cab3f801e6ad88830dec4ada6f88e1ce9
Eric <lzhang.eric@gmail.com>
2017-02-05 12:28:11 -0500

    1, Add and enable Flask-moment 2, Add icon

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html'), 200
 
 @app.errorhandler(404)
 def page_not_found(e):
