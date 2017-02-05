0872a011a403bbc6df810f297d659572cc80f684
Eric <lzhang.eric@gmail.com>
2017-02-05 17:20:57 -0500

    Add and enable Flask-wtf

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Popeyes is better then KFC.'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html'), 200
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data= ''
+    return render_template('index.html', form=form, name=name), 200
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -20,9 +30,9 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('505.html', error=e), 500
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+class NameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[Required()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     manager.run()
