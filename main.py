6839973538701f3d3e3bcb3ae55098c7a9df4a76
Eric <lzhang.eric@gmail.com>
2017-02-04 19:39:24 -0500

    Add some settings.

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return '<h1>Hello World</h1>', 200
+
+@app.route('/User/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>' % name, 200
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
