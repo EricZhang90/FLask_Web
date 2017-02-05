3541aa52062bfbfedea110869918bcd2c1473d75
Eric <lzhang.eric@gmail.com>
2017-02-04 19:19:44 -0500

    no message

--- main.py
+++ main.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World</h1>'
 
-@app.route('/User/<name>')
-def User(name):
-    return '<h1>Hello, %s!</h1>' % name
-
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
