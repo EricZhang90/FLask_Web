d6bd2ed537db50ff6d84d9411a88c5b62bd82faa
Eric <lzhang.eric@gmail.com>
2017-02-05 21:47:36 -0500

    1, Add redirect 2, Add alert 3, Separate class NameForm

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+
+from NameForm import NameForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Popeyes is better then KFC.'
@@ -18,9 +17,12 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data= ''
-    return render_template('index.html', form=form, name=name), 200
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name.')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -28,11 +30,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('505.html', error=e), 500
-
-class NameForm(FlaskForm):
-    name = StringField("What's your name?", validators=[Required()])
-    submit = SubmitField('Submit')
+    return render_template('500.html', error=e), 500
 
 if __name__ == '__main__':
     manager.run()
