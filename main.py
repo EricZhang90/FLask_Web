897f6db87fb12c7b6f177ccf2342e3343917a139
Eric <lzhang.eric@gmail.com>
2017-02-06 21:57:33 -0500

    Add db, migrate and shell manager.

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 from NameForm import NameForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Popeyes is better then KFC.'
 
-manager = Manager(app)
+# db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:4166666906@localhost/PythonWeb'
+app.config['SQLAlCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 bootstrap = Bootstrap(app)
+
 moment = Moment(app)
+
+# manager
+manager = Manager(app)
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name.')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] =True
+
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                           form=form,
+                           name=session.get('name'),
+                           know=session.get('known', False))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -31,6 +58,27 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html', error=e), 500
+
+
+# models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 if __name__ == '__main__':
     manager.run()
