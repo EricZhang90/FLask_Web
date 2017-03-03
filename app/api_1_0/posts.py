3d551dbb4966cd4a882135dcbff14ec0e2e34cc7
ericzhang90 <lzhang.eric@gmail.com>
2017-03-03 15:05:39 +0000

    RESTful API: add pagination

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -1,6 +1,6 @@
 from . import api
 from .authentication import auth
-from flask import jsonify, g, request, url_for
+from flask import jsonify, g, request, url_for, current_app
 from ..models import Post, Permission
 from .decorators import permission_required
 from .. import db
@@ -11,8 +11,22 @@ from errors import forbidden
 @api.route('/posts/')
 @auth.login_required
 def get_posts():
-    posts = Post.query.all()
-    return jsonify({'posts': [post.to_json() for post in posts]})
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.paginate(page,
+                                     per_page=current_app.config['PW_POSTS_PER_PAGE='],
+                                     error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page+1, _external=True)
+    return jsonify({'prev': prev,
+                    'next': next,
+                    'count': pagination.total,
+                    'posts': [post.to_json() for post in posts]
+                    })
 
 
 @api.route('/post/<int:id>')
@@ -43,6 +57,8 @@ def edit_post(id):
     db.session.add(post)
     db.session.commit()
     return jsonify(post.to_json())
+
+
 
 
 
