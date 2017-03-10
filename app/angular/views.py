ebc76e31a3b0037384050cce615d90d54b94d354
ericzhang90 <lzhang.eric@gmail.com>
2017-03-10 18:45:47 +0000

    Add new Jinja+Anuglar example

--- app/angular/views.py
+++ app/angular/views.py
@@ -1,6 +1,15 @@
 from flask import render_template
 from . import angular
+from ..models import User, Post
+import time
 
 @angular.route('/')
 def index():
-    return render_template('angular/index.html')
+    admin = User.query.filter_by(username='Main_Tester').first()
+    posts = [{
+              'body': posts.body,
+              'timestamp': str(time.mktime(posts.timestamp.timetuple()))
+             }
+                for posts in admin.posts.order_by(Post.timestamp.desc()).all()]
+
+    return render_template('angular/index.html', posts=posts)
